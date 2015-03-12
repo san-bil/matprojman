753f5f2768f37908c8f9b18667a2c115f23e88c9
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-12 20:47:57 +0000

    changed backticks in installer

--- install/install.sh
+++ install/install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo $0
-
 echo "Creating $HOME/bin/matprojman folder..."
 mkdir -p $HOME/bin/matprojman
-echo "cp -R `readlink -f \`dirname \`dirname $0\`\``/* $HOME/bin/matprojman/"
+echo "cp -R $( readlink -f $( dirname $( dirname $0 ) ) )/* $HOME/bin/matprojman/"
 
 echo "Installing requirements..."
 pip install --user -r $HOME/bin/matprojman/requirements.txt
 
 echo "Add the following to your .zshrc or .bashrc file (or whatever analogue for your own shell)"
-cat `readlink -f \`dirname \`dirname $0\`\``/.add_path_example.txt
+cat $(readlink -f $( dirname $( dirname $0 ) ) )/.add_path_example.txt
 
 
