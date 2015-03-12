4531dd38bf25ac66b08e08835dbc0c091a50b336
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-12 21:05:16 +0000

    fixed bug

--- install/install.sh
+++ install/install.sh
@@ -8,8 +8,8 @@ echo -e "Installing requirements...\n"
 pip install --user -r $HOME/bin/matprojman/requirements.txt
 
 echo -e "Add the following to your .zshrc or .bashrc file (or whatever analogue for your own shell)\n"
-echo -e "########################################################\n\n"
-cat $( dirname -f $( dirname $( readlink -f $0 ) ) )/.add_path_example.txt
+echo -e "########################################################\n"
+cat $( dirname  $( dirname $( readlink -f $0 ) ) )/.add_path_example.txt
 echo -e "\n########################################################\n\n"
 
 
