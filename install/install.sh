de40a5a430438f3e1c32829922e9623a358eeb01
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-12 20:50:38 +0000

    formatting£

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Creating $HOME/bin/matprojman folder..."
+echo  "Creating $HOME/bin/matprojman folder..."
 mkdir -p $HOME/bin/matprojman
 cp -R $( readlink -f $( dirname $( dirname $0 ) ) )/* $HOME/bin/matprojman/
 
-echo "Installing requirements..."
+echo -e "Installing requirements...\n"
 pip install --user -r $HOME/bin/matprojman/requirements.txt
 
-echo "Add the following to your .zshrc or .bashrc file (or whatever analogue for your own shell)"
+echo -e "Add the following to your .zshrc or .bashrc file (or whatever analogue for your own shell)\n"
 cat $(readlink -f $( dirname $( dirname $0 ) ) )/.add_path_example.txt
 
 
