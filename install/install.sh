cb7ff1fd51e33a069145afa1be8bb015f19bbd67
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-12 21:00:55 +0000

    switched dirname and readlink to fix bug when running installer from its own folder

--- install/install.sh
+++ install/install.sh
@@ -2,14 +2,14 @@
 
 echo  "Creating $HOME/bin/matprojman folder..."
 mkdir -p $HOME/bin/matprojman
-cp -R $( readlink -f $( dirname $( dirname $0 ) ) )/* $HOME/bin/matprojman/
+cp -R $( dirname $( dirname $( readlink -f $0 ) ) )/* $HOME/bin/matprojman/
 
 echo -e "Installing requirements...\n"
 pip install --user -r $HOME/bin/matprojman/requirements.txt
 
 echo -e "Add the following to your .zshrc or .bashrc file (or whatever analogue for your own shell)\n"
 echo -e "########################################################\n\n"
-cat $(readlink -f $( dirname $( dirname $0 ) ) )/.add_path_example.txt
+cat $( dirname -f $( dirname $( readlink -f $0 ) ) )/.add_path_example.txt
 echo -e "########################################################\n\n"
 
 
